5a1aa8b1f0addece01e796ac782efe5eabed3613
Thao <thaobube@gmail.com>
2021-04-14 23:15:03 +0200

    basic environment setting

---
+++ dependances.bat
@@ -0,0 +1,8 @@
+composer require annotations
+composer require symfony/orm-pack
+composer require --dev symfony/maker-bundle
+composer require symfony/apache-pack
+composer require symfony/maker-bundle --dev
+composer require --dev doctrine/doctrine-fixtures-bundle
+
+
